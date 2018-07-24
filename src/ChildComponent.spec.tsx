4e13840c146f606f420800ba02dadcd26b0facb4
Adam Gordon <adam.gordon@workday.com>
2018-07-24 11:18:56 -0600

    add tests using react-testing-library

--- src/ChildComponent.spec.tsx
+++ src/ChildComponent.spec.tsx
@@ -1,10 +1,23 @@
 import * as React from 'react'
 import { create } from 'react-test-renderer'
+import { cleanup, render } from 'react-testing-library'
+
 import { ChildComponent } from 'ChildComponent'
 
 describe('test the <ChildComponent/> component', () => {
-  test('it should render the child component', () => {
-    const tree = create(<ChildComponent />).toJSON()
-    expect(tree).toMatchSnapshot()
+  describe('using react-test-renderer', () => {
+    test('it should render the child component', () => {
+      const tree = create(<ChildComponent />).toJSON()
+      expect(tree).toMatchSnapshot()
+    })
+  })
+
+  describe('using react-testing-library', () => {
+    afterEach(cleanup)
+
+    test('it should render the child component', () => {
+      const { container } = render(<ChildComponent />)
+      expect(container.firstChild).toMatchSnapshot()
+    })
   })
 })
--- src/ParentComponent.spec.tsx
+++ src/ParentComponent.spec.tsx
@@ -1,26 +1,47 @@
 import * as React from 'react'
 import { create } from 'react-test-renderer'
+import { cleanup, render } from 'react-testing-library'
 
 import { ChildComponent } from 'ChildComponent'
 import { ParentComponent } from 'ParentComponent'
 import { stubComponent } from 'test/utils'
 
 describe('test the <ParentComponent/> component', () => {
-  test('it should deep render the parent component', () => {
-    const tree = create(<ParentComponent />).toJSON()
-    expect(tree).toMatchSnapshot()
-  })
+  describe('using react-test-renderer', () => {
+    test('it should deep render the parent component', () => {
+      const tree = create(<ParentComponent />).toJSON()
+      expect(tree).toMatchSnapshot()
+    })
 
-  describe('test the <ParentComponent/> component with stubbing', () => {
-    stubComponent(ChildComponent)
-    test('it should shallow render the parent component', () => {
+    describe('test the <ParentComponent/> component with stubbing', () => {
+      stubComponent(ChildComponent)
+      test('it should shallow render the parent component', () => {
+        const tree = create(<ParentComponent />).toJSON()
+        expect(tree).toMatchSnapshot()
+      })
+    })
+
+    test('it should deep render the parent component again', () => {
       const tree = create(<ParentComponent />).toJSON()
       expect(tree).toMatchSnapshot()
     })
   })
 
-  test('it should deep render the parent component again', () => {
-    const tree = create(<ParentComponent />).toJSON()
-    expect(tree).toMatchSnapshot()
+  describe('using react-testing-library', () => {
+    afterEach(cleanup)
+
+    test('it should deep render the parent component', () => {
+      const { container } = render(<ParentComponent />)
+      expect(container.firstChild).toMatchSnapshot()
+    })
+
+    describe('test the <ParentComponent/> component with stubbing', () => {
+      stubComponent(ChildComponent)
+
+      test('it should shallow render the parent component again', () => {
+        const { container } = render(<ParentComponent />)
+        expect(container.firstChild).toMatchSnapshot()
+      })
+    })
   })
 })
